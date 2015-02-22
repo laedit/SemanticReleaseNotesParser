ce8e7fa7d25a944b76df12c2e12cbf23223785f3
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-02-22 22:38:08 +0100

    Fix tests on AppVeyor

--- SemanticReleaseNotesParser.Core.Tests/SemanticReleaseNotesFormatterTest.cs
+++ SemanticReleaseNotesParser.Core.Tests/SemanticReleaseNotesFormatterTest.cs
@@ -268,187 +268,34 @@ namespace SemanticReleaseNotesParser.Core.Tests
             };
         }
 
-        private const string ExampleAHtml = @"<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>
-<ul>
-<li>{New} Release Checker: Now gives you a breakdown of exactly what you are missing.</li>
-<li>{New} Structured Layout: An alternative layout engine that allows developers to control layout.</li>
-<li>{Changed} Timeline: Comes with an additional grid view to show the same data.</li>
-<li>{Fix} Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>
-</ul>
+        private const string ExampleAHtml = "<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n<ul>\r\n<li>{New} Release Checker: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li>{New} Structured Layout: An alternative layout engine that allows developers to control layout.</li>\r\n<li>{Changed} Timeline: Comes with an additional grid view to show the same data.</li>\r\n<li>{Fix} Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>\r\n</ul>\r\n\r\n</body></html>";
 
-</body></html>";
+        private const string ExampleAMarkdown = "Incremental release designed to provide an update to some of the core plugins.\r\n - {New} Release Checker: Now gives you a breakdown of exactly what you are missing.\r\n - {New} Structured Layout: An alternative layout engine that allows developers to control layout.\r\n - {Changed} Timeline: Comes with an additional grid view to show the same data.\r\n - {Fix} Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.";
 
-        private const string ExampleAMarkdown = @"Incremental release designed to provide an update to some of the core plugins.
- - {New} Release Checker: Now gives you a breakdown of exactly what you are missing.
- - {New} Structured Layout: An alternative layout engine that allows developers to control layout.
- - {Changed} Timeline: Comes with an additional grid view to show the same data.
- - {Fix} Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.";
+        private const string ExampleAHtmlCategories = "<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n<h1>New</h1>\r\n<ul>\r\n<li>Release Checker: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li>Structured Layout: An alternative layout engine that allows developers to control layout.</li>\r\n</ul>\r\n<h1>Changed</h1>\r\n<ul>\r\n<li>Timeline: Comes with an additional grid view to show the same data.</li>\r\n</ul>\r\n<h1>Fix</h1>\r\n<ul>\r\n<li>Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>\r\n</ul>\r\n\r\n</body></html>";
 
-        private const string ExampleAHtmlCategories = @"<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>
-<h1>New</h1>
-<ul>
-<li>Release Checker: Now gives you a breakdown of exactly what you are missing.</li>
-<li>Structured Layout: An alternative layout engine that allows developers to control layout.</li>
-</ul>
-<h1>Changed</h1>
-<ul>
-<li>Timeline: Comes with an additional grid view to show the same data.</li>
-</ul>
-<h1>Fix</h1>
-<ul>
-<li>Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>
-</ul>
+        private const string ExampleAMarkdownCategories = "Incremental release designed to provide an update to some of the core plugins.\r\n# New\r\n - Release Checker: Now gives you a breakdown of exactly what you are missing.\r\n - Structured Layout: An alternative layout engine that allows developers to control layout.\r\n# Changed\r\n - Timeline: Comes with an additional grid view to show the same data.\r\n# Fix\r\n - Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.";
 
-</body></html>";
+        private const string ExampleBHtml = "<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n<h1>System</h1>\r\n<ul>\r\n<li>{New} <em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li>{New} <em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>\r\n</ul>\r\n<h1>Plugin</h1>\r\n<ul>\r\n<li>{Changed} <em>Timeline</em>: Comes with an additional grid view to show the same data.</li>\r\n<li>{Fix} <em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>\r\n</ul>\r\n\r\n</body></html>";
 
-        private const string ExampleAMarkdownCategories = @"Incremental release designed to provide an update to some of the core plugins.
-# New
- - Release Checker: Now gives you a breakdown of exactly what you are missing.
- - Structured Layout: An alternative layout engine that allows developers to control layout.
-# Changed
- - Timeline: Comes with an additional grid view to show the same data.
-# Fix
- - Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.";
+        private const string ExampleBMarkdown = "Incremental release designed to provide an update to some of the core plugins.\r\n# System\r\n - {New} *Release Checker*: Now gives you a breakdown of exactly what you are missing.\r\n - {New} *Structured Layout*: An alternative layout engine that allows developers to control layout.\r\n# Plugin\r\n - {Changed} *Timeline*: Comes with an additional grid view to show the same data.\r\n - {Fix} *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement.";
 
-        private const string ExampleBHtml = @"<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>
-<h1>System</h1>
-<ul>
-<li>{New} <em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>
-<li>{New} <em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>
-</ul>
-<h1>Plugin</h1>
-<ul>
-<li>{Changed} <em>Timeline</em>: Comes with an additional grid view to show the same data.</li>
-<li>{Fix} <em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>
-</ul>
+        private const string CustomLiquidTemplate = "\r\n{%- for category in categories -%}\r\n# {{ category.name }}\r\n{%- for item in category.items -%}\r\n - {{ item.summary }}\r\n{%- endfor -%}\r\n{%- endfor -%}\r\n\r\n{{ release_notes.summary }}";
 
-</body></html>";
+        private const string CustomLiquidTemplateHtml = "<html><body><h1>New</h1>\r\n<ul>\r\n<li>Release Checker: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li>Structured Layout: An alternative layout engine that allows developers to control layout.</li>\r\n</ul>\r\n<h1>Changed</h1>\r\n<ul>\r\n<li>Timeline: Comes with an additional grid view to show the same data.</li>\r\n</ul>\r\n<h1>Fix</h1>\r\n<ul>\r\n<li>Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>\r\n</ul>\r\n<p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n\r\n</body></html>";
 
-        private const string ExampleBMarkdown = @"Incremental release designed to provide an update to some of the core plugins.
-# System
- - {New} *Release Checker*: Now gives you a breakdown of exactly what you are missing.
- - {New} *Structured Layout*: An alternative layout engine that allows developers to control layout.
-# Plugin
- - {Changed} *Timeline*: Comes with an additional grid view to show the same data.
- - {Fix} *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement.";
+        private const string CustomLiquidTemplateMarkdown = "# New\r\n - Release Checker: Now gives you a breakdown of exactly what you are missing.\r\n - Structured Layout: An alternative layout engine that allows developers to control layout.\r\n# Changed\r\n - Timeline: Comes with an additional grid view to show the same data.\r\n# Fix\r\n - Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.\r\n\r\nIncremental release designed to provide an update to some of the core plugins.";
 
-        private const string CustomLiquidTemplate = @"
-{%- for category in categories -%}
-# {{ category.name }}
-{%- for item in category.items -%}
- - {{ item.summary }}
-{%- endfor -%}
-{%- endfor -%}
+        private const string ExampleCHtml = "<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n<ul>\r\n<li><em>Example</em>: You can have global issues that aren't grouped to a section</li>\r\n</ul>\r\n<h1>System</h1>\r\n<ul>\r\n<li>{new} <em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li>{new} <em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>\r\n</ul>\r\n<h1>Plugin</h1>\r\n<ul>\r\n<li>{Changed} <em>Timeline</em>: Comes with an additional grid view to show the same data.</li>\r\n<li>{Fix} <em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement. <a href=\"http://getglimpse.com\">i1234</a></li>\r\n</ul>\r\n\r\n</body></html>";
 
-{{ release_notes.summary }}";
+        private const string ExampleCMarkdown = "Incremental release designed to provide an update to some of the core plugins.\r\n - *Example*: You can have global issues that aren't grouped to a section\r\n# System\r\n - {new} *Release Checker*: Now gives you a breakdown of exactly what you are missing.\r\n - {new} *Structured Layout*: An alternative layout engine that allows developers to control layout.\r\n# Plugin\r\n - {Changed} *Timeline*: Comes with an additional grid view to show the same data.\r\n - {Fix} *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement. [i1234](http://getglimpse.com)";
 
-        private const string CustomLiquidTemplateHtml = @"<html><body><h1>New</h1>
-<ul>
-<li>Release Checker: Now gives you a breakdown of exactly what you are missing.</li>
-<li>Structured Layout: An alternative layout engine that allows developers to control layout.</li>
-</ul>
-<h1>Changed</h1>
-<ul>
-<li>Timeline: Comes with an additional grid view to show the same data.</li>
-</ul>
-<h1>Fix</h1>
-<ul>
-<li>Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.</li>
-</ul>
-<p>Incremental release designed to provide an update to some of the core plugins.</p>
+        private const string ExampleCHtmlCategories = "<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n<ul>\r\n<li><em>Example</em>: You can have global issues that aren't grouped to a section</li>\r\n</ul>\r\n<h1>New</h1>\r\n<ul>\r\n<li><em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li><em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>\r\n</ul>\r\n<h1>Changed</h1>\r\n<ul>\r\n<li><em>Timeline</em>: Comes with an additional grid view to show the same data.</li>\r\n</ul>\r\n<h1>Fix</h1>\r\n<ul>\r\n<li><em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement. <a href=\"http://getglimpse.com\">i1234</a></li>\r\n</ul>\r\n\r\n</body></html>";
 
-</body></html>";
+        private const string ExampleCMarkdownCategories = "Incremental release designed to provide an update to some of the core plugins.\r\n - *Example*: You can have global issues that aren't grouped to a section\r\n# New\r\n - *Release Checker*: Now gives you a breakdown of exactly what you are missing.\r\n - *Structured Layout*: An alternative layout engine that allows developers to control layout.\r\n# Changed\r\n - *Timeline*: Comes with an additional grid view to show the same data.\r\n# Fix\r\n - *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement. [i1234](http://getglimpse.com)";
 
-        private const string CustomLiquidTemplateMarkdown = @"# New
- - Release Checker: Now gives you a breakdown of exactly what you are missing.
- - Structured Layout: An alternative layout engine that allows developers to control layout.
-# Changed
- - Timeline: Comes with an additional grid view to show the same data.
-# Fix
- - Ajax: Fix that crashed poll in Chrome and IE due to log/trace statement.
+        private const string ExampleDHtml = "<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>\r\n<ul class=\"srn_priority\">\r\n<li data-content=\"1\"><em>Example</em>: You can have global issues that aren't grouped to a section</li>\r\n</ul>\r\n<h1>System</h1>\r\n<ul class=\"srn_priority\">\r\n<li data-content=\"3\">{new} <em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>\r\n<li data-content=\"2\">{new} <em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>\r\n</ul>\r\n<h1>Plugin</h1>\r\n<ul class=\"srn_priority\">\r\n<li data-content=\"1\">{Changed} <em>Timeline</em>: Comes with an additional grid view to show the same data.</li>\r\n<li data-content=\"1\">{Fix} <em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement. <a href=\"http://getglimpse.com\">i1234</a></li>\r\n</ul>\r\n\r\n</body></html>";
 
-Incremental release designed to provide an update to some of the core plugins.";
-
-        private const string ExampleCHtml = @"<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>
-<ul>
-<li><em>Example</em>: You can have global issues that aren't grouped to a section</li>
-</ul>
-<h1>System</h1>
-<ul>
-<li>{new} <em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>
-<li>{new} <em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>
-</ul>
-<h1>Plugin</h1>
-<ul>
-<li>{Changed} <em>Timeline</em>: Comes with an additional grid view to show the same data.</li>
-<li>{Fix} <em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement. <a href=""http://getglimpse.com"">i1234</a></li>
-</ul>
-
-</body></html>";
-
-        private const string ExampleCMarkdown = @"Incremental release designed to provide an update to some of the core plugins.
- - *Example*: You can have global issues that aren't grouped to a section
-# System
- - {new} *Release Checker*: Now gives you a breakdown of exactly what you are missing.
- - {new} *Structured Layout*: An alternative layout engine that allows developers to control layout.
-# Plugin
- - {Changed} *Timeline*: Comes with an additional grid view to show the same data.
- - {Fix} *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement. [i1234](http://getglimpse.com)";
-
-        private const string ExampleCHtmlCategories = @"<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>
-<ul>
-<li><em>Example</em>: You can have global issues that aren't grouped to a section</li>
-</ul>
-<h1>New</h1>
-<ul>
-<li><em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>
-<li><em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>
-</ul>
-<h1>Changed</h1>
-<ul>
-<li><em>Timeline</em>: Comes with an additional grid view to show the same data.</li>
-</ul>
-<h1>Fix</h1>
-<ul>
-<li><em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement. <a href=""http://getglimpse.com"">i1234</a></li>
-</ul>
-
-</body></html>";
-
-        private const string ExampleCMarkdownCategories = @"Incremental release designed to provide an update to some of the core plugins.
- - *Example*: You can have global issues that aren't grouped to a section
-# New
- - *Release Checker*: Now gives you a breakdown of exactly what you are missing.
- - *Structured Layout*: An alternative layout engine that allows developers to control layout.
-# Changed
- - *Timeline*: Comes with an additional grid view to show the same data.
-# Fix
- - *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement. [i1234](http://getglimpse.com)";
-
-        private const string ExampleDHtml = @"<html><body><p>Incremental release designed to provide an update to some of the core plugins.</p>
-<ul class=""srn_priority"">
-<li data-content=""1""><em>Example</em>: You can have global issues that aren't grouped to a section</li>
-</ul>
-<h1>System</h1>
-<ul class=""srn_priority"">
-<li data-content=""3"">{new} <em>Release Checker</em>: Now gives you a breakdown of exactly what you are missing.</li>
-<li data-content=""2"">{new} <em>Structured Layout</em>: An alternative layout engine that allows developers to control layout.</li>
-</ul>
-<h1>Plugin</h1>
-<ul class=""srn_priority"">
-<li data-content=""1"">{Changed} <em>Timeline</em>: Comes with an additional grid view to show the same data.</li>
-<li data-content=""1"">{Fix} <em>Ajax</em>: Fix that crashed poll in Chrome and IE due to log/trace statement. <a href=""http://getglimpse.com"">i1234</a></li>
-</ul>
-
-</body></html>";
-
-        private const string ExampleDMarkdown = @"Incremental release designed to provide an update to some of the core plugins.
- 1. *Example*: You can have global issues that aren't grouped to a section
-# System
- 3. {new} *Release Checker*: Now gives you a breakdown of exactly what you are missing.
- 2. {new} *Structured Layout*: An alternative layout engine that allows developers to control layout.
-# Plugin
- 1. {Changed} *Timeline*: Comes with an additional grid view to show the same data.
- 1. {Fix} *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement. [i1234](http://getglimpse.com)";
+        private const string ExampleDMarkdown = "Incremental release designed to provide an update to some of the core plugins.\r\n 1. *Example*: You can have global issues that aren't grouped to a section\r\n# System\r\n 3. {new} *Release Checker*: Now gives you a breakdown of exactly what you are missing.\r\n 2. {new} *Structured Layout*: An alternative layout engine that allows developers to control layout.\r\n# Plugin\r\n 1. {Changed} *Timeline*: Comes with an additional grid view to show the same data.\r\n 1. {Fix} *Ajax*: Fix that crashed poll in Chrome and IE due to log/trace statement. [i1234](http://getglimpse.com)";
     }
 }
