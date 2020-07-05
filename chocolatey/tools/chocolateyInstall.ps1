731875d15fc2479ce9f71e6cb39b5e9763539af0
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2020-07-05 09:12:26 +0200

    Fix build and add netcore version in zip. Upload and publish artifacts on release.

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = 'SemanticReleaseNotesParser'
-$url = 'https://github.com/laedit/SemanticReleaseNotesParser/releases/download/v[version]/SemanticReleaseNotesParser.[version].zip'
+$url = 'https://github.com/laedit/SemanticReleaseNotesParser/releases/download/v[version]/SemanticReleaseNotesParser.win-x86.[version].zip'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- local-build.ps1
+++ local-build.ps1
@@ -3,6 +3,10 @@ If (Test-Path artifacts) {
 }
 New-Item -ItemType directory -Path artifacts
 
+If (Test-Path publish) {
+    Remove-Item -Path publish -recurse
+}
+
 # Install dependencies
 dotnet tool restore
 dotnet restore src
@@ -15,27 +19,29 @@ $version = dotnet gitversion /output json /showvariable NuGetVersionV2
 "Version: $version"
 
 # Build
+Remove-Item -Path src/**/bin -recurse
 dotnet build src --configuration Release --no-restore /p:Version=$version
 
 # Vika [TODO]
-# vika --debug --includesource
+# vika --debug --includesource static-analysis.Core.sarif.json static-analysis.Program.sarif.json
+
+# Build release notes
+dotnet run --project src/SemanticReleaseNotesParser --no-build --configuration Release -- -g=categories --debug -o="artifacts/ReleaseNotes.html" --pluralizecategoriestitle --includestyle
 
 # Test
 dotnet test src --configuration Release --no-build --nologo --verbosity normal --collect:"XPlat Code Coverage"
 dotnet reportgenerator "-reports:src/**/TestResults/**/coverage.cobertura.xml" "-targetdir:artifacts/reports" -reporttypes:Html
 
-# Publish windows version
-dotnet publish src/SemanticReleaseNotesParser --configuration Release --output ./publish --self-contained true --runtime win-x86 -p:PublishSingleFile=true -p:PublishTrimmed=true /p:PackAsTool=false /p:Version=$version
-
-# Build release notes
-./publish/SemanticReleaseNotesParser.exe -g=categories --debug -o="artifacts/ReleaseNotes.html" --pluralizecategoriestitle --includestyle
-
 # Create packages
+Compress-Archive -Path src/SemanticReleaseNotesParser/bin/Release/netcoreapp3.1/*, artifacts/ReleaseNotes.html -DestinationPath "artifacts/SemanticReleaseNotesParser.netcore.$version.zip"
 Copy-Item -Path "src/SemanticReleaseNotesParser/bin/Release/SemanticReleaseNotesParser.$version.nupkg" -Destination artifacts
 Copy-Item -Path "src/SemanticReleaseNotesParser.Core/bin/Release/SemanticReleaseNotesParser.Core.$version.nupkg" -Destination artifacts
 Copy-Item -Path "src/SemanticReleaseNotesParser.Core/bin/Release/SemanticReleaseNotesParser.Core.$version.snupkg" -Destination artifacts
+
+# Publish windows version
+dotnet publish src/SemanticReleaseNotesParser --configuration Release --output ./publish --self-contained true --runtime win-x86 -p:PublishSingleFile=true -p:PublishTrimmed=true /p:PackAsTool=false /p:Version=$version
 Copy-Item -Path "artifacts/ReleaseNotes.html" -Destination publish
-Compress-Archive -Path publish/* -DestinationPath "artifacts/SemanticReleaseNotesParser.$version.zip"
+Compress-Archive -Path publish/* -DestinationPath "artifacts/SemanticReleaseNotesParser.win-x86.$version.zip"
 
 if (choco -v) {
     $installPath = "chocolatey/tools/chocolateyInstall.ps1"
